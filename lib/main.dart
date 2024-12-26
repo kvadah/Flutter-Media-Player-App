b6723fa53cf4292a8cf880d68e768afcd9ba87d3
khalliid9 <kvadah9@gmail.com>
2024-12-26 17:02:01 +0300

    asking for permission to read videos and ausios

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 void main() {
   runApp(const VideoFolders());
@@ -12,14 +13,57 @@ class VideoFolders extends StatefulWidget {
 }
 
 class _VideoFoldersState extends State<VideoFolders> {
+  List<String> videoFolders = ['khalid'];
+  bool hasPermission = true;
+
+  @override
+  void initState() {
+    ();
+    requestAudioAndVideoPermission();
+    super.initState();
+  }
+
+  
+
+  Future<void> requestAudioAndVideoPermission() async {
+    // Check location permission
+    PermissionStatus status = await Permission.audio.request();
+    if (status.isGranted) {
+    } else {
+      setState(() {});
+    }
+     PermissionStatus status1 = await Permission.videos.request();
+     if (status1.isGranted) {
+    } else {
+      setState(() {});
+    }
+     PermissionStatus status2 = await Permission.storage.request();
+      if (status2.isGranted) {
+    } else {
+      setState(() {});
+    }
+  }
+
+ 
+  Future<void> requestStoragePermission() async {
+    // Check location permission
+    PermissionStatus status = await Permission.storage.request();
+    if (status.isGranted) {
+    } else {
+      setState(() {
+        "Location permission denied";
+      });
+    }
+  }
+
+  // Fetch video folders after permission is granted
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: const Text('Videos'),
-        ),
-        body: const Column(
-          children: [Center(child: Text('Videos folders'))],
-        ));
+    return const MaterialApp(
+      home: Scaffold(
+        body: Center(child: Text('khalid')),
+      ),
+    );
   }
 }
